4595b5aec59c385250dc9f3fd23707edf06a9482
Abdelkader <gaddour000@gmail.com>
2021-05-26 13:39:47 +0100

    video call with mute and videoOff

--- server.js
+++ server.js
@@ -3,13 +3,43 @@ const morgan = require("morgan");
 const connectDB = require("./config/db");
 const bodyParser = require("body-parser");
 const cors = require("cors");
+const { render } = require("ejs");
+const app = express();
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
+const { ExpressPeerServer } = require("peer");
+const peerServer = ExpressPeerServer(server, {
+  debug: true,
+});
+app.use("/peerjs", peerServer);
+const { v4: uuidV4 } = require("uuid");
 // Config dotev
 require("dotenv").config({
   path: "./config/config.env",
 });
 
-const app = express();
+//---------- videoChat setup start ---------
+app.set("view engine", "ejs");
+app.use(express.static("public"));
 
+app.get("/", (req, res) => {
+  res.redirect(`/${uuidV4()}`);
+});
+app.get("/:room", (req, res) => {
+  res.render("room", { roomId: req.params.room });
+});
+
+io.on("connection", (socket) => {
+  socket.on("join-room", (roomId, userId) => {
+    socket.join(roomId);
+    socket.broadcast.to(roomId).emit("user-connected", userId);
+
+    socket.on("disconnect", () => {
+      socket.broadcast.to(roomId).emit("user-disconnected", userId);
+    });
+  });
+});
+//---------- videoChat setup end -----------
 // Connect to database
 connectDB();
 
@@ -42,6 +72,6 @@ app.use((req, res) => {
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
 });
